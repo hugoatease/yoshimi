d011a778401b0f7801bb7ef404a385a461a29004
Hugo Caille <hugo.caille@orange.fr>
2015-08-30 19:00:44 +0200

    Password change UI

--- client/account.js
+++ client/account.js
@@ -147,6 +147,75 @@ var Email = React.createClass({
   }
 });
 
+var Password = React.createClass({
+  getInitialState: function() {
+    return {
+      error: null,
+      success: null
+    }
+  },
+
+  submit: function(ev) {
+    ev.preventDefault();
+    request.post('/api/user/password')
+      .send({
+        current_password: React.findDOMNode(this.refs.current_password).value,
+        password: React.findDOMNode(this.refs.password).value,
+        password_confirm: React.findDOMNode(this.refs.password_confirm).value
+      })
+      .end(function(err, res) {
+        if (err) {
+          this.setState({error: res.body.message, success: null});
+        }
+        else {
+          this.setState({success: res.text, error: null});
+        }
+      }.bind(this));
+  },
+
+  render: function() {
+    var error = null, success = null;
+    if (this.state.error) {
+      var error = (
+        <div className="alert alert-warning"><b>Error. </b>{this.state.error}</div>
+      )
+    }
+
+    if (this.state.success) {
+      var success = (
+        <div className="alert alert-success"><b>Success. </b>{this.state.success}</div>
+      )
+    }
+
+    return (
+      <div className="panel panel-default">
+        <div className="panel-heading">Password change</div>
+        <div className="panel-body">
+          {error}
+          {success}
+          <form onSubmit={this.submit}>
+            <div className="form-group">
+              <label htmlFor="current_password">Current password</label>
+              <input className="form-control" type="password" id="current_password" ref="current_password" placeholder="Current password" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input className="form-control" type="password" id="password" ref="password" placeholder="Password" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password_confirm">Password confirmation</label>
+              <input className="form-control" type="password" id="password_confirm" ref="password_confirm" placeholder="Password confirmation" />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-default">Change password</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
+
 module.exports = React.createClass({
   render: function() {
     return (
@@ -155,9 +224,10 @@ module.exports = React.createClass({
         <div className="row">
           <div className="col-md-6">
             <Profile />
+            <Email />
           </div>
           <div className="col-md-6">
-            <Email />
+            <Password />
           </div>
         </div>
       </div>
